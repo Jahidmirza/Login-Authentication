b45e0d92af192b4282b20b8e09a289403b6d6dbf
Jahid Mirza <Jahidmirza22@gmail.com>
2021-10-18 20:16:39 +0600

    ok

---
+++ firebase.js
@@ -0,0 +1,12 @@
+//  Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCN3yjwRF3txiAY76WsnZoAZoCA9K1vqzo",
+    authDomain: "login-form-b012e.firebaseapp.com",
+    projectId: "login-form-b012e",
+    storageBucket: "login-form-b012e.appspot.com",
+    messagingSenderId: "355311502585",
+    appId: "1:355311502585:web:069e66710f7f7450f20e28"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
